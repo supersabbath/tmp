1aa6d5ce15dac9cc0327badf4343266833fc33ab
Fernando <fernando.canon@playco.com>
2015-05-18 12:07:32 +0200

    Initial Commit

---
+++ StarzPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarzPlayer
+//
+//  Created by Fernando Canon on 18/05/15.
+//  Copyright (c) 2015 Fernando Canon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StarzPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StarzPlayer
+//
+//  Created by Fernando Canon on 18/05/15.
+//  Copyright (c) 2015 Fernando Canon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
