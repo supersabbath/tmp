3cc2491402a41b7763062a4bb6b0052f3bb06486
Fernando <fernando.canon@playco.com>
2015-06-24 16:27:57 +0200

    refactorin player, added new wrapper class to adobe player

---
+++ StarzPlayer/src/SPMoviePlayerController.h
@@ -0,0 +1,13 @@
+//
+//  SPMoviePlayerController.h
+//  StarzPlayer
+//
+//  Created by Fernando Canon on 24/06/15.
+//  Copyright (c) 2015 Fernando Canon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPMoviePlayerController : NSObject
+
+@end
---
+++ StarzPlayer/src/SPMoviePlayerController.m
@@ -0,0 +1,13 @@
+//
+//  SPMoviePlayerController.m
+//  StarzPlayer
+//
+//  Created by Fernando Canon on 24/06/15.
+//  Copyright (c) 2015 Fernando Canon. All rights reserved.
+//
+
+#import "SPMoviePlayerController.h"
+
+@implementation SPMoviePlayerController
+
+@end
